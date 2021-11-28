74102d4e7d242e7fdd8ac43cf8320dd7647a64a9
Simon Christiansen <sich97@gmail.com>
2021-11-28 04:49:49 +0100

    Rewrote the data structure
    Next step: Rewrite the calculator to fit the new data structure

--- data_structure.py
+++ data_structure.py
@@ -1,101 +1,373 @@
+import random
+
+# TODO: Missing power resource (in the entire local script as well)
+COMMS_NAME: str = "Comms"
+NAVS_NAME: str = "Navs"
+DATA_NAME: str = "Data"
+
+HEAT_NAME: str = "Heat"
+DRIFT_NAME: str = "Drift"
+THRUST_NAME: str = "Thrust"
+
+
+class Resource:
+    """
+    The base class for all resource types, such as Comms, Navs, Data, Heat, Drift, Thrust
+    """
+
+    name: str
+    value: int
+    min_value: int
+    max_value: int
+
+    def __init__(self, name: str, value: int = 0, min_value: int = 0, max_value: int = 999):
+        self.name: str = name
+        self.value: int = value
+        self.min_value: int = min_value
+        self.max_value: int = max_value
+
+    def is_valid_value(self) -> bool:
+        """
+        Checks if the numerical validity of the resource value based on init parameters
+        """
+        return self.min_value <= self.value <= self.max_value
+
+
 class Command:
+    """
+    Contains a ratio for exchanging input resources into output resources
+    """
+
     name: str
     input_resources: dict[str, int]
     output_resources: dict[str, int]
 
     def __init__(self, name: str, input_resources: dict[str, int], output_resources: dict[str, int]):
-        self.name = name
-        self.input_resources = input_resources.copy()
-        self.output_resources = output_resources.copy()
+        self.name: str = name
+        self.input_resources: dict[str, int] = input_resources.copy()
+        self.output_resources: dict[str, int] = output_resources.copy()
 
-    def __str__(self):
-        output = self.name + " [Command]" + "\n\t"\
-                 + "Input resources: " + str(self.input_resources) + "\n\t"\
-                 + "Output resources: " + str(self.output_resources) + "\n"
+    def __str__(self) -> str:
+        output: str = self.name + " [Command]" + "\n\t" \
+                      + "Input resources: " + str(self.input_resources) + "\n\t" \
+                      + "Output resources: " + str(self.output_resources) + "\n"
         return remove_trailing_newlines(output)
 
-    def copy(self):
+    def copy(self) -> type(__name__):
         return type(self)(self.name, self.input_resources.copy(), self.output_resources.copy())
 
 
 class Turn:
-    commands: list[Command]
-    max_command_amount: int
+    """
+    Contains a set of commands up to a maximum specified amount.
+    Also contains a dictionary of the current resources, which is the result of executing all the commands in sequence,
+    starting with a set of starting recourses specified in the init. This current resources dictionary gets updated each
+    time a new command is appended to this object.
+    Whenever the last possible command is appended (determined by the maximum specified in init),
+    the next_turn function gets called on every item in current_resources, which is populated by subclasses of Resource.
+    """
 
-    def __init__(self, commands: list[Command], max_command_amount: int = None):
-        if max_command_amount:
-            self.max_command_amount = max_command_amount
+    max_commands: int
+    current_resources: dict[str, type(Resource)] = {}
+    commands: list[Command] = []
 
-        if self.max_command_amount:
-            if len(commands) > self.max_command_amount:
-                raise AttributeError(
-                    "Error when attempting to add command to Turn object beyond its specified max command amount!")
-        self.commands = commands.copy()
+    def __init__(self, starting_resources: dict[str, type(Resource)],
+                 max_commands: int, commands: list[Command] = None):
 
-    def __str__(self):
-        output = "[Turn]: "
+        if commands is None:
+            commands: list[Command] = []
+
+        self.max_commands: int = max_commands
+
+        for starting_resource_name, starting_resource in starting_resources.items():
+            self.current_resources[starting_resource_name]: Resource = starting_resource
+
+        if len(commands) < self.max_commands:
+            self.commands: list[Command] = commands
+
+    def __str__(self) -> str:
+        output: str = "[Turn]: "
         for command in self.commands:
             output += "\n" + indent_string(str(command))
         output += "\n"
         return remove_trailing_newlines(output)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.commands)
 
-    def append(self, command: Command):
-        if self.max_command_amount:
-            if len(self.commands) + 1 > self.max_command_amount:
+    def copy(self) -> type(__name__):
+        return type(self)(self.current_resources.copy(), self.max_commands, commands=self.commands.copy())
+
+    def append(self, command: Command) -> bool:
+        """
+        As long as there is room for one more, this function appends a given command to the local list of commands and
+        applies the command's changes to the resource pool (current_resources).
+        If, after appending this command, the length of local commands has reached the specified limit, the turn is
+        considered complete, and the next_turn function is run on every resource in self.current_resources.
+        """
+        # Check if room for one more
+        if len(self.commands) < self.max_commands:
+
+            # Append command
+            self.commands.append(command)
+
+            # Apply changes to resource pool (current_resources)
+            for resource_name, resource_value in command.input_resources.items():
+                self.current_resources[resource_name].value += resource_value
+                if not self.current_resources[resource_name].is_valid_value():
+                    return False
+            for resource_name, resource_value in command.output_resources.items():
+                self.current_resources[resource_name].value += resource_value
+                if not self.current_resources[resource_name].is_valid_value():
+                    return False
+
+            # If turn is complete
+            if len(self.commands) == self.max_commands:
+                return self.apply_end_of_turn_effects()
+
+            # If not room for one more
+            else:
                 raise AttributeError(
                     "Error when attempting to add command to Turn object beyond its specified max command amount!")
-        self.commands.append(command.copy())
+
+    def apply_end_of_turn_effects(self) -> bool:
+        """
+        Calls the next_turn on every resource in current_resources.
+        # TODO: Prevent this from being run after the last turn is complete (if this indeed matches the game)
+        """
+        for resource_name, resource in self.current_resources.items():
+            if not resource.next_turn():
+                return False
         return True
 
-    def copy(self):
-        return type(self)(self.commands.copy(), self.max_command_amount)
+    def is_valid(self) -> bool:
+        """
+        Checks the validity of every resource value in current_resources.
+        """
+        for resource in self.current_resources.values():
+            if not resource.is_valid_value():
+                return False
+        return True
 
 
 class Route:
-    turns = []
-    max_turn_amount = None
+    """
+    Contains a set of turns up to a maximum specified amount.
+    Also contains a reference to the last turn's current_resources. Since every turn appended to a Route is supposed to
+    already be complete, the last turn's current_resources is assumed to be the resulting resources from all the turns
+    contained in this route.
+    A route is considered valid if all turns within the Route is considered valid.
+    It's considered finished if it contains an amount of turns equal to the max_turns specified in init, although this
+    has to be checked from outside this class.
 
-    def __init__(self, turns: list[Turn], max_turn_amount: int = None):
-        if max_turn_amount:
-            self.max_turn_amount = max_turn_amount
+    If a Route is found to be finished, elsewhere in the script, we may call this class' satisfies_objectives function
+    to see if this Route indeed is to be considered a candidate for the player's choices.
 
-        if self.max_turn_amount:
-            if len(turns) > self.max_turn_amount:
-                raise AttributeError(
-                    "Error when attempting to add turn to Route object beyond its specified max turn amount!")
-        self.turns = turns.copy()
+    Yet to be implemented, this class will also contain the get_possible_turns function, which will serve to provide
+    a list of turns possible based on the route's current_resources
+    (which is the current - or rather soon to be previous, turn's current_resources).
+    """
 
-    def __str__(self):
-        output = "[Route]: "
+    max_turns: int
+    current_resources: dict[str, type(Resource)] = {}
+    turns: list[Turn] = []
+
+    def __init__(self, turns: list[Turn], max_turns: int):
+        self.max_turns: int = max_turns
+        for turn in turns:
+            self.append(turn)
+
+    def __str__(self) -> str:
+        output: str = "[Route]: "
         for turn in self.turns:
             output += "\n" + indent_string(str(turn))
         output += "\n"
         return remove_trailing_newlines(output)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.turns)
 
-    def append(self, turn: Turn):
-        if self.max_turn_amount:
-            if not len(self.turns) + 1 > self.max_turn_amount:
-                raise AttributeError(
-                    "Error when attempting to add turn to Route object beyond its specified max turn amount!")
-        self.turns.append(turn.copy())
+    def copy(self) -> type(__name__):
+        return type(self)(self.turns.copy(), self.max_turns)
+
+    def append(self, turn) -> bool:
+        """
+        Appends the specified turn to the Route, as long as there's room.
+        Updates the Route's current_resources to the specified turn's current_resources.
+        """
+        if len(self.turns) < self.max_turns:
+            self.turns.append(turn)
+            self.current_resources = self.turns[-1].current_resources
+            return self.is_valid()
+        else:
+            raise AttributeError(
+                "Error when attempting to add turn to Route object beyond its specified max turn amount!")
+
+    def is_valid(self) -> bool:
+        for turn in self.turns:
+            if not turn.is_valid():
+                return False
         return True
 
-    def copy(self):
-        return type(self)(self.turns.copy(), self.max_turn_amount)
+    def is_finished(self, objective) -> bool:
+        return len(self.turns) == self.max_turns and self.satisfies_objective(objective)
+
+    def satisfies_objective(self, objective_resources) -> bool:
+        """
+        TODO: Implement bonus objectives
+        """
+        for objective_resource_name, objective_resource_value in objective_resources.items():
+            if not self.current_resources[objective_resource_name].value >= objective_resource_value:
+                return False
+        return True
+
+    def get_possible_turns(self) -> list[Turn]:
+        """
+        TODO: Create this function
+        """
+        pass
+
+
+class Heat(Resource):
+    """
+    This subclass of Resource, contains variables and methods specific to this particular in-game resource.
+    """
+
+    overheat_limit: int
+    min_random_increase: int
+    max_random_increase: int
+
+    def __init__(self, overheat_limit: int, min_increase: int, max_increase: int):
+        super().__init__(HEAT_NAME, max_value=overheat_limit)
+        self.overheat_limit: int = overheat_limit
+        self.min_random_increase: int = min_increase
+        self.max_random_increase: int = max_increase
+
+    def next_turn(self) -> bool:
+        """
+        Heat is a resource that gains a random amount after each turn.
+        TODO: Does Heat gain this random amount when the very last turn is submitted?
+        """
+        if self.is_valid_value:
+            self.value += random.randint(self.min_random_increase, self.max_random_increase)
+            if self.value < self.overheat_limit:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def is_valid_end_of_route(self) -> bool:
+        """
+        The task fails if it has too much heat.
+        """
+        return self.is_valid_value and self.value < self.overheat_limit
+
+
+class Drift(Resource):
+    """
+    This subclass of Resource, contains variables and methods specific to this particular in-game resource.
+    """
+    drift_bounds: list[int]
+
+    def init(self, drift_bounds: list[int], min_drift: int, max_drift: int):
+        super().__init__(DRIFT_NAME, min_value=min_drift, max_value=max_drift)
+        self.drift_bounds = drift_bounds
+
+    def next_turn(self) -> bool:
+        return self.is_valid_value()
+
+    def is_valid_end_of_route(self) -> bool:
+        """
+        The task fails if there is too little or too much drift at the end of the last turn.
+        # TODO: Does the game care at all about drift before the very end?
+        """
+        return self.drift_bounds[0] <= self.value <= self.drift_bounds[1]
+
+
+class Thrust(Resource):
+    """
+    This subclass of Resource, contains variables and methods specific to this particular in-game resource.
+    """
+    required_thrust: int
+
+    def init(self, max_thrust: int, required_thrust: int):
+        super().__init__(THRUST_NAME, max_value=max_thrust)
+        self.required_thrust = required_thrust
+
+    def next_turn(self) -> bool:
+        """
+        Some thrust is lost at the end of each turn (but not after the very last one)
+        It can't go below 0
+        # TODO: Implement the above description, if it indeed matches with the game.
+        """
+        return self.is_valid_value()
+
+    def is_valid_end_of_route(self) -> bool:
+        """
+        The task fails if there isn't enough thrust at the end. In this sense, it acts just like a regular resource.
+        With the exception that some thrust is automatically lost each turn.
+        """
+        return self.is_valid_value() and self.value >= self.required_thrust
+
+
+class Comms(Resource):
+    """
+    This subclass of Resource, contains variables and methods specific to this particular in-game resource.
+    This is what's considered a regular resource.
+    """
+    def __init__(self):
+        super().__init__(COMMS_NAME)
+
+    def next_turn(self) -> bool:
+        return self.is_valid_value()
+
+    def is_valid_end_of_route(self) -> bool:
+        return self.is_valid_value()
+
+
+class Navs(Resource):
+    """
+    This subclass of Resource, contains variables and methods specific to this particular in-game resource.
+    This is what's considered a regular resource.
+    """
+    def __init__(self):
+        super().__init__(NAVS_NAME)
+
+    def next_turn(self) -> bool:
+        return self.is_valid_value()
+
+    def is_valid_end_of_route(self) -> bool:
+        return self.is_valid_value()
+
+
+class Data(Resource):
+    """
+    This subclass of Resource, contains variables and methods specific to this particular in-game resource.
+    This is what's considered a regular resource.
+    """
+    def __init__(self):
+        super().__init__(DATA_NAME)
+
+    def next_turn(self) -> bool:
+        return self.is_valid_value()
+
+    def is_valid_end_of_route(self) -> bool:
+        return self.is_valid_value()
 
 
 def indent_string(string: str) -> str:
-    new_string = "\t" + string.replace("\n", "\n\t")
+    """
+    Indents every line within a string, including the first one.
+    """
+    new_string: str = "\t" + string.replace("\n", "\n\t")
     return new_string
 
 
 def remove_trailing_newlines(string: str) -> str:
+    """
+    Removes trailing newlines, except the very last one.
+    """
     for i in range(len(string) - 1, -1, -1):
         if not string[i] == "\n":
             return string[:i+2]
